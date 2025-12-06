6b61cd7f1516857bd01333511f8c119322835586
ashish-kus <ashish.kus@gmail.com>
2025-12-06 12:37:35 +0530

    fix docker error

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,10 @@ PDF_PATH="/app/Ashish_Kushwaha.pdf"
 PDF_NAME="Ashish_Kushwaha.pdf"
 
 # Upload to S3 (requires environment variables)
+# Upload to S3 (requires environment variables)
 if [ -n "$S3_BUCKET" ]; then
-  echo "Uploading PDF to S3 bucket: $S3_BUCKET"
-  aws s3 cp "$PDF_PATH" "s3://$S3_BUCKET/$PDF_NAME" \
-    --acl public-read \
-    --content-type "application/pdf"
-  echo "PDF uploaded to S3 bucket successfully."
-else
-  echo "Warning: S3_BUCKET not set. Skipping S3 upload."
+  aws s3 cp $PDF_NAME s3://$S3_BUCKET/$PDF_NAME
+  echo "PDF uploaded to S3 Bucker. "
 fi
 
 # Run Pinecone ingestion
